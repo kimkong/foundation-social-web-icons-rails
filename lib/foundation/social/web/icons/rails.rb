a449e3c52490258a400db8013c104ff02dfa6de9
Kenny Kim <kennyhkim@gmail.com>
2014-05-06 09:42:16 -0700

    adding lib

---
+++ lib/foundation/social/web/icons/rails.rb
@@ -0,0 +1,17 @@
+require "foundation/social/web/icons/version"
+
+module Foundation
+  module Social
+    module Web
+      module Icons
+        module Rails
+
+          def self.ipsum
+            "Lorem ipsum dolor sit amet, consectetur adipisicing ...."
+
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/foundation/social/web/icons/rails/version.rb
@@ -0,0 +1,11 @@
+module Foundation
+  module Social
+    module Web
+      module Icons
+        module Rails
+          VERSION = "0.0.1"
+        end
+      end
+    end
+  end
+end
